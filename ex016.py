339604f4b41797fa8a68a5c74c62105de3022f20
Filippe Xavier <filippe_natan@hotmail.com>
2022-03-16 15:11:15 -0300

    Exercício 016. Porção inteira.

---
+++ ex016.py
@@ -0,0 +1,4 @@
+from math import trunc
+num = float(input('Informe um número para descobrir a porção inteira:'))
+numTruncado = trunc(num)
+print('A porção inteira de {} é {}'.format(num, numTruncado))
