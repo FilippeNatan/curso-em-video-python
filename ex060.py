87a57cbf9127917908fd3b6a022e784a13bddb0c
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-15 17:06:46 -0300

    Exercício 060. Informe o número que deseja saber o fatorial.

---
+++ ex060.py
@@ -0,0 +1,10 @@
+numero = int(input('Informe um número que deseja saber o fatorial: '))
+retornoNumero = numero
+fatorial = numero
+
+while numero != 1:
+    multi = fatorial * (numero - 1)
+    fatorial = multi
+    numero -= 1
+
+print('O fatorial de {} é {}'.format(retornoNumero, fatorial))
