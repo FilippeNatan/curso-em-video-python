b4f662064ad9b9d6bd20a3b1d2f0438bba5d2077
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-15 14:18:31 -0300

    Exercício 055. Ler o peso de 5 pessoas e mostrar o maior e o menor.

--- ex055.py
+++ ex055.py
@@ -1,8 +1,7 @@
 numPessoas = 5
 arrPeso = []
 
-for contador in range(numPessoas):
-    peso = int(input('Informe o peso de {}° pessoa: '.format(contador+1)))#gambi
-    # tentei iniciar a variavel com um, mas não deu certo
+for contador in range(1, numPessoas+1):#coloquei +1 pois o contador inicia em 0
+    peso = int(input('Informe o peso de {}° pessoa: '.format(contador)))
     arrPeso.append(peso)
 print('O maior peso é {} Kg e menor peso é {} Kg'.format(max(arrPeso, key=int),min(arrPeso, key=int)))
