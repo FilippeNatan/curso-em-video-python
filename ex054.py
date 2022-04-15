53216012d6ef9b87834fa330aec271e053de1063
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-15 14:03:01 -0300

    Alteração de while para for

--- ex054.py
+++ ex054.py
@@ -1,14 +1,14 @@
-import datetime
+from datetime import date
 
-date = datetime.date.today()
+date = date.today()
 numPessoas = 7
 contador = 1
 numPessoasMaioridade = 0
 numPessoasMinoridade = 0
 
 print('Informe a data de nascimento de {} pessoas.'.format(numPessoas))
-while(contador <= numPessoas):
-    anoPessoa = int(input('Informe a o ano de nascimento de {}º pessoa: '.format(contador)))
+for pessoa in range(0, numPessoas):
+    anoPessoa = int(input('Informe a o ano de nascimento de {}ª pessoa: '.format(contador)))
     if(date.year - anoPessoa >= 21):
         numPessoasMaioridade+=1
     else:
