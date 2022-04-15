bd17fb8f559f86abc80e5aa94a9aa4e09dc2d0e1
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-15 14:52:42 -0300

    Exercício 056. Ler os dados de 4 pessoas e mostrar o nome do homem mais velho e a quantidade de mulheres com menos de 20 anos.

--- ex056.py
+++ ex056.py
@@ -1,18 +1,18 @@
 from time import sleep
 
-nomes = []; idades = []; sexo = []; numPessoa = 2; homemMaiorIdade = 0; homemMaiorIdadeNome = 'Ninguém'; mulheresMenos20Anos = 0
+nomes = []; idades = []; sexo = []; numPessoa = 4; homemMaiorIdade = 0; homemMaiorIdadeNome = 'Ninguém'; mulheresMenos20Anos = 0
 
 for contador in range(numPessoa):
 
     nomes.append(input('Informe o nome da {}º pessoa: '.format(contador + 1)))
     idades.append(int(input('Informe a idade da {}º pessoa: '.format(contador + 1))))
-    sexo.append(input('Informe o sexo da {}º pessoa(m ou f): '.format(contador + 1)))
+    sexo.append(input('Informe o sexo da {}º pessoa(M ou F): '.format(contador + 1)))
 
-    if (sexo[contador] == 'm' and idades[contador] > homemMaiorIdade):
+    if (sexo[contador] in 'Mm' and idades[contador] > homemMaiorIdade):
         homemMaiorIdade = idades[contador]
         homemMaiorIdadeNome = nomes[contador]
 
-    if(sexo[contador] == 'f' and idades[contador] < 20):
+    if(sexo[contador] in 'Ff' and idades[contador] < 20):
         mulheresMenos20Anos+=1
 
 somaIdades = sum(idades) / numPessoa
