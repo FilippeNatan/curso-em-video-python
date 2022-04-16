3add26fc220448f184f2a2cea49c24407100fe43
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-16 16:24:21 -0300

    Exercício 058. Jogo de adivinhação, informe um número de 1 a 10.

--- ex058.py
+++ ex058.py
@@ -8,12 +8,15 @@ print('Tente me vencer! Pensei em um número entre 0 e 10...')
 numUsuario = int(input('Tente adivinhar qual é:'))
 while numComputador != numUsuario:
     print('PROCESSANDO...')
-    sleep(3)
+    sleep(1)
     tentativas += 1
     print('Você errou! Que pena!')
+    if numUsuario < numComputador:
+        print('É um número maior...')
+    if numComputador < numUsuario:
+        print('É um número menor...')
     numUsuario = int(input('Tente novamente: '))
-else:
-    print('PROCESSANDO...')
-    sleep(3)
-    tentativas += 1
-    print('Você acertou! Parabéns! O número era: {} e você precisou de {} tentativas.'.format(numComputador, tentativas))
+print('PROCESSANDO...')
+sleep(3)
+tentativas += 1
+print('Você acertou! Parabéns! O número era: {} e você precisou de {} tentativas.'.format(numComputador, tentativas))
