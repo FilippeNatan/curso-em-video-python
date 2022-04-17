4acef0ee2c75b6f15624816bda7cd269128b43b5
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-17 19:42:40 -0300

    Exercício 068. Jogo par ou impar, só para quando vc perde.

--- ex068.py
+++ ex068.py
@@ -4,9 +4,11 @@ print('|*|'*10)
 print('{:^30}'.format('VAMOS JOGAR PAR OU IMPAR'))
 print('|*|'*10)
 
+vitorias = 0
+
 while True:
     n = int(input('Diga um número: '))
-    while n not in range(0,10):
+    while n not in range(0,11):
         n = int(input('Diga um número: '))
     v = str(input('Escolha Par ou Ímpar: [P/I]')).upper().strip()
     while v not in 'PI':
@@ -19,15 +21,17 @@ while True:
     else:
         string = 'DEU ÍMPAR'
     if s % 2 == 0 and v == 'P':
+        vitorias += 1
         print(f'Você jogou {n} e o computador jogou {c}. Total de {s} {string}.')
         print('Você VENCEU!\n'
               'Vamos jogar novamente...')
     elif s % 2 != 0 and v == 'I':
+        vitorias += 1
         print(f'Você jogou {n} e o computador jogou {c}. Total de {s} {string}.')
         print('Você VENCEU!\n'
               'Vamos jogar novamente...')
     else:
         print(f'Você jogou {n} e o computador jogou {c}. Total de {s} {string}.')
         print('Você PERDEU!\n'
-              'Obrigado por jogar...')
+              f'Obrigado por jogar... Você teve {vitorias} vitória(s)!')
         break
