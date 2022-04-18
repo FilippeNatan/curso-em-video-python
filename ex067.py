3d9e11cbde09ab93606a3ef7483d6628b4e351d4
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-18 20:28:24 -0300

    Exercício 067. Tabuada 2.0 sem números negativos.

--- ex067.py
+++ ex067.py
@@ -1,9 +1,8 @@
-n = p = 0
-while n >= 0:
+while True:
     n = int(input('Quer ver a tabuada de qual valor?'))
     if n < 0:
         break
-    for t in range(1, 10):
+    for t in range(1, 11):
         p = n * t
         print(f'{n} x {t} = {p}')
 print('TABUADA ENCERRADA! Não multiplicamos negativos aqui! Volte sempre!')
