031152d805577cf11fa641ba9d84752000739fd4
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-19 15:23:06 -0300

    Exercício 072. Ler um número do teclado e mostrar escrito por extenso. Usando tupla e sem if.

--- ex072.py
+++ ex072.py
@@ -1,5 +1,7 @@
-tuplaNumeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove',
-                'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinte', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+tuplaNumeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco',
+                'Seis', 'Sete', 'Oito', 'Nove',
+                'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze',
+                'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 numeroEscolhido = int(input('Escolha um número de 0 a 20: '))
 while numeroEscolhido not in range(0, 21):
     numeroEscolhido = int(input('Opção inválida. Escolha um número de 0 a 20: '))
