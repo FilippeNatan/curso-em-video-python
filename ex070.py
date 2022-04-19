71238f9c4c5b0796d85869dba66dc284fd11b56c
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-18 21:25:34 -0300

    Exercício 070. Cadastro de produtos e mostrar qual produto tem o menor preço, quantos custam mais de 1000 e o total gasto.

--- ex070.py
+++ ex070.py
@@ -5,24 +5,20 @@ while True:
     nomeProduto = str(input('Informe o nome do produto: ')).strip()
     while nomeProduto == '':
         nomeProduto = str(input('Informe o nome do produto: ')).strip()
-    precoProduto = int(input('Informe o preço do produto: '))
+    precoProduto = float(input('Informe o preço do produto: R$ '))
     while precoProduto < 0:
-        precoProduto = int(input('Informe o preço do produto: '))
+        precoProduto = float(input('Informe o preço do produto: R$ '))
     totalGasto += precoProduto
     if precoProduto > 1000:
         produtosMais1000 += 1
-    if primeiraVez == True:
-        primeiraVez == False
+    if primeiraVez == True or precoProduto < precoMaisBarato:
         nomeMaisBarato = nomeProduto
         precoMaisBarato = precoProduto
-    if precoProduto < precoMaisBarato:
-        nomeMaisBarato = nomeProduto
-        precoMaisBarato = precoProduto
-    querContinuar = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+    querContinuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
     while querContinuar not in 'SNsn':
-        querContinuar = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+        querContinuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
     if querContinuar in 'Nn':
         break
-print(f'O total gasto foi de R$ {totalGasto},00\n'
+print(f'O total gasto foi de R$ {totalGasto:.2f}\n'
       f'{produtosMais1000} produto(s) custam mais de R$ 1000,00\n'
-      f'{nomeMaisBarato} é o produto mais barato e seu preço é R$ {precoMaisBarato},00')
+      f'{nomeMaisBarato} é o produto mais barato e seu preço é R$ {precoMaisBarato:.2f}')
