af149a9c05408f2ce51ebe0b5ae69c6a67a5d02b
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-19 13:01:46 -0300

    Exercício 076. Tabular uma lista de preços vinda de uma tupla.

---
+++ ex076.py
@@ -0,0 +1,9 @@
+tuplaProdutos = ('Pão', 10, 'Leite', 8, 'Ovos', 15, 'Frango', 13, 'Biscoito', 4)
+print('-'*40)
+print('{:^40}'.format('LISTA DE PREÇOS'))
+print('-'*40)
+for num in tuplaProdutos:
+    if tuplaProdutos.index(num) % 2 == 0:
+        print(f'{tuplaProdutos[tuplaProdutos.index(num)]:30}R$', end=' ')
+    else:
+        print(f'{tuplaProdutos[tuplaProdutos.index(num)]:.2f}')
