e239a89f59bef27f443e1f48aaa4f9ccc507518d
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-19 11:42:26 -0300

    Exercício 074. Gerar uma tupla com 5 números aleatórios e mostrar qual foi o menor e o maior.

---
+++ ex074.py
@@ -0,0 +1,9 @@
+from random import randint
+
+tuplaNumerosAleatorios = (randint(0, 99), randint(0, 99), randint(0, 99), randint(0, 99), randint(0, 99))
+
+print(f'Sorteio de Números: {tuplaNumerosAleatorios}')
+
+print(f'O maior número foi: {max(tuplaNumerosAleatorios)}')
+
+print(f'O menor número foi: {min(tuplaNumerosAleatorios)}')
