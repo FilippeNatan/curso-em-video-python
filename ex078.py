01b8339ec2f326e0639c962b6aac2ebb8423d65c
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-20 14:20:39 -0300

    Exercício 078. Receber 5 valores, colocar em uma lista e retornar o maior o menor e as posições.

---
+++ ex078.py
@@ -0,0 +1,25 @@
+valores = []; listaMaiores = [0]; listaMenores = [0];
+posicoesMaiores = []; posicoesMenores = []; primeiraVez = True
+
+for n in range(0, 5):
+    valores.append(int(input(f'Digite o {n+1}º valor: ')))
+    if valores[n] == max(listaMaiores):
+        listaMaiores.append(valores[n])
+    if valores[n] > max(listaMaiores):
+        del listaMaiores[0:]
+        listaMaiores.append(valores[n])
+    if valores[n] == min(listaMenores):
+        listaMenores.append((valores[n]))
+    if valores[n] < min(listaMenores) or primeiraVez == True:
+        primeiraVez = False
+        del listaMenores[0:]
+        listaMenores.append(valores[n])
+for i, n in enumerate(valores):
+    if n in listaMaiores:
+        posicoesMaiores.append(i)
+for i, n in enumerate(valores):
+    if n in listaMenores:
+        posicoesMenores.append(i)
+print(f'Você digitou {valores}')
+print(f'O maior valor digitado foi: {max(valores)} nas posições {posicoesMaiores}')
+print(f'O menor valor digitado foi: {min(valores)} nas posições {posicoesMenores}')
