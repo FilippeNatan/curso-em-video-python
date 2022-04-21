7eeac48cbc12ddefe75f2a22fc00f8c5819a7a31
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-21 14:05:34 -0300

    Exercício 081. Receber valores e retornar uma lista ordenada decrescente, validar se há o valor 5 e o número de elementos digitados.

---
+++ ex081.py
@@ -0,0 +1,17 @@
+valores = []
+while True:
+    valor=int(input('Digite um valor: '))
+    valores.append(valor)
+    querContinuar=str(input('Quer continuar: [S/N]')).upper().strip()[0]
+    if querContinuar not in 'SN':
+        print('Opção inválida!')
+        querContinuar = str(input('Quer continuar: [S/N]'))
+    if querContinuar == 'N':
+        break
+print('-='*30)
+print(f'Você digitou {len(valores)} elementos.')
+print(f'Os valores em ordem decrescente são {sorted(valores, reverse=True)}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não faz parte da lista!')
