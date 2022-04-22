e655ace5f0d4997605a6bb57fa480c26bdc98898
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-22 14:15:12 -0300

    Exercício 080. Receber 5 valores e colocar em uma lista ordenada crescente, sem usar sort.

--- ex080.py
+++ ex080.py
@@ -1,13 +1,16 @@
 valores = []
 for n in range(0, 5):
     valor = int(input('Digite um valor: '))
-    if n == 0:
+    if n == 0 or valor > valores[-1]:
         valores.append(valor)
         print('Adicionado ao final da lista')
     else:
-        for i, v in enumerate(valores):
-            if valor > valores(v):
-                valores.append((valor))
-            elif valor < valores(v):
-                valores.insert(i, valor)
-print(valores)
+        pos = 0
+        while pos < len(valores):
+            if n <= valores[pos]:
+                valores.insert(pos, n)
+                print(f'Adicionado a posição {pos} da lista')
+                break
+            pos += 1
+print('-='*30)
+print(f'Os valores digitados em ordem foram {valores}')
---
+++ ex083.py
@@ -0,0 +1,6 @@
+expressao=[]
+valor=input('Digite uma expressão: ')
+expressao.append(valor)
+for n in len(valor):
+    print(valor[n])
+    n += 1
