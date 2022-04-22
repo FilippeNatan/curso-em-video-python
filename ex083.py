2727a5104542a3e3c7030f3c7d79dd0460ea0784
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-22 17:33:39 -0300

    Exercício 0883. Receber uma expressão e dizer se é válida ou inválida.

--- ex083.py
+++ ex083.py
@@ -1,6 +1,11 @@
-expressao=[]
-valor=input('Digite uma expressão: ')
-expressao.append(valor)
-for n in len(valor):
-    print(valor[n])
-    n += 1
+abrindo = 0; fechando = 0
+expressao = (str(input('Digite uma expressão: ')))
+for n in expressao:
+    if n in '(':
+        abrindo += 1
+    elif n in ')':
+        fechando += 1
+if abrindo == fechando:
+    print('Sua expressão é válida')
+elif abrindo != fechando:
+    print('Sua expressão é inválida')
