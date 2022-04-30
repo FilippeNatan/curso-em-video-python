184385b24f136a41e3d875bc6814b3c9b17260d5
Filippe Xavier <filippe_natan@hotmail.com>
2022-04-30 16:36:58 -0300

    Exercício 089. Alunos com número e médias

--- ex089.py
+++ ex089.py
@@ -1,17 +1,24 @@
-boletim = []; contador = 0; numeroAluno = 0
+boletim = []
 
 while True:
     nome = str(input('Nome: '))
-    nota1 = int(input('Nota 1: '))
-    nota2 = int(input('Nota 2: '))
-    boletim.append([numeroAluno])
-    boletim.append([nome])
-    boletim.append([nota1, nota2])
-    numeroAluno += 1
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2)/2
+    boletim.append([nome, [nota1, nota2], media])
     querContinuar = str(input('Quer continuar? [S/N]'))[0].strip().upper()
     while querContinuar not in 'SN':
         querContinuar = str(input('Quer continuar? [S/N]')).strip().upper()
     if querContinuar == 'N':
         break
-print('Nº NOME            MÉDIA')
-print('-'*30)
+print(f'{"Nº":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-'*26)
+for i, a in enumerate(boletim):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+    if i+1 == len(boletim):
+        while True:
+            consulta = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+            if consulta == 999:
+                break
+            print(f'Notas de {boletim[consulta][0]} foram {boletim[consulta][1]}')
+print('Obrigado e volte sempre!')
