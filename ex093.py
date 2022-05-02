73f1fce39d0e61d2c8c29ef0bfa25bad878e54ee
Filippe Xavier <filippe_natan@hotmail.com>
2022-05-02 17:22:31 -0300

    Exercício 093. Dados de jogador de futebol, quantos gols fez, total e quantas partidas.

---
+++ ex093.py
@@ -0,0 +1,19 @@
+dadosJogador = {}
+listaGols = []
+
+dadosJogador['nome'] = str(input('Nome do jogador: '))
+numeroPartidas = int(input('Quantas partidas o jogador jogou?'))
+for n in range(1, numeroPartidas+1):
+    listaGols.append(int(input(f'Quantos gols na partida {n}? ')))
+dadosJogador['gols'] = listaGols
+dadosJogador['total'] = sum(listaGols)
+print('-=-'*20)
+print(dadosJogador)
+print('-=-'*20)
+for i, v in dadosJogador.items():
+    print(f'O campo {i} tem o valor {v}.')
+print('-=-'*20)
+print(f'O jogador {dadosJogador["nome"]} jogou {numeroPartidas} partidas.')
+for i, v in enumerate(listaGols):
+    print(f'Na partida {i}, fez {v} gols.')
+print(f'Foi um total de {dadosJogador["total"]} gols.')
