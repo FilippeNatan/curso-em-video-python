490c920e3be6d6fa18c71d971c68ba02775d774a
Filippe Xavier <filippe_natan@hotmail.com>
2022-05-03 12:34:33 -0300

    Exercício 0883. Receber uma expressão e dizer se é válida ou inválida.

---
+++ ex085.py
@@ -0,0 +1,9 @@
+valores = [[], []]
+for n in range(1, 8):
+    v = (int(input(f'Digite o {n}º valor: ')))
+    if v % 2 == 0:
+        valores[0].append(v)
+    else:
+        valores[1].append(v)
+print(f'Os valores pares digitados em ordem crescente foram: {sorted(valores[0])}')
+print(f'Os valores ímpares digitados em ordem crescente foram: {sorted(valores[1])}')
